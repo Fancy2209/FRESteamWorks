f933193a6cb0d4631c7333c421b44f85b739e42a
Fancy2209 <64917206+Fancy2209@users.noreply.github.com>
2025-07-11 18:07:47 +0000

    aefdsfdssfd

--- config.sh
+++ config.sh
@@ -4,18 +4,12 @@
 # be used in the Makefile.
 
 # Path to the main AIR SDK directory, so that $AIR_SDK/bin/adt exists.
-if [[ -z "$AIR_SDK" ]]; then 
-    AIR_SDK="${HOME}/SDKS/AIRSDK"
-fi
+AIR_SDK="${HOME}/SDKS/AIRSDK"
 # Path to the Flex SDK, so that $FLEX_SDK/bin/compc exists.
 # Might be identical to AIR_SDK if you use an overlayed SDK.
-if [[ -z "$FLEX_SDK" ]]; then 
-    FLEX_SDK="${HOME}/SDKS/AIRSDK"
-fi
+FLEX_SDK="${HOME}/SDKS/AIRSDK"
 # Path to the Steam SDK, so that $STEAM_SDK/redistributable_bin exists.
-if [[ -z "$STEAM_SDK" ]]; then 
-    STEAM_SDK="${HOME}/SDKS/SteamSDK"
-fi
+STEAM_SDK="${HOME}/SDKS/SteamSDK"
 # FTP path to upload the built binaries to when running builds/build.sh.
 # Version number gets appended to the path. Binaries will not be uploaded
 # if left commented or set to an empty value.
